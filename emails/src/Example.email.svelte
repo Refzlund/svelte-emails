6d768a521230e6d766ef5e29f756fec749addb85
Arthur @Refzlund <arthur@refzlund.com>
2025-12-11 18:07:42 +0100

    fix(iframe): improve content height calculation and add color picker docs (#16)

---
+++ emails/src/Example.email.svelte
@@ -0,0 +1,12 @@
+<script lang='ts'>
+
+	import { Email, Div, Text } from 'svelte-emails'
+	
+</script>
+
+<Email preview="Welcome!" body-bg-[#ebebeb] bg-[#ffffff]>
+  <Div p-6 bg-[#ffffff]>
+    <Text.H1 content="Hello World" />
+    <Text content="This is my first email." />
+  </Div>
+</Email>
